696a8aa510f6828646ac6df61550867dd7e28a44
ThePortNetwork <122316803+ThePortNetwork@users.noreply.github.com>
2023-01-13 21:13:24 +0000

    Add files via upload

---
+++ EncodeUserName.py
@@ -0,0 +1,5 @@
+str= input("Enter Username: ").encode('utf-8')
+hex_str = str.hex()
+print(f"Encoded Username: {hex_str}")
+notes = open("username.txt", 'a')
+notes.write(f"{note}")
---
+++ GetUserName.py
@@ -0,0 +1,5 @@
+def run():
+ with open('username.txt', 'r') as f:
+  for word in f:
+   username = bytearray.fromhex(word).decode()
+   return username
